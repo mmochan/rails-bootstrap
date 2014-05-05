cdc54a5c1eb8e6811b043ec950566d0952c44f13
Mike Mochan <mmochan@mac.com>
2014-05-06 07:34:46 +1000

    updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
+
 RailsBootstrap::Application.routes.draw do
-  root :to => 'visitors#new'
+  root :to => 'visitors#index'
 
   resources :webtiers
   resources :apptiers
   resources :servicetiers
   resources :dbtiers
-=======
-Rails.application.routes.draw do
-  root :to => "visitors#index"
->>>>>>> 7932994d0cd42068da90919e0d72fdefdf214f35
 end
